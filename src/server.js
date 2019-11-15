ead4280aef991bc3eab0b2808327ec39928f646b
Gabriel Henrique <g_henriqueferreira@hotmail.com>
2019-11-15 13:58:17 -0300

    Config PORT heroku

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const tasks = require('./config/tasks');
 const cors = require('cors');
 
-//dotenv.config();
+dotenv.config();
 
 const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://hasf:gsagml20@hasf-gofom.mongodb.net/backend?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -22,4 +22,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false })); //used for /boleto/postback_url
 app.use(routes); 
 
-app.listen(process.env.SERVER_PORT)
+app.listen(process.env.PORT || 4444)
